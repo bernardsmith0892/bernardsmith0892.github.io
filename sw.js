ab836dd138e8cbc5fe306a86ab8179a48f12abfa
bernardsmith0892 <bernard.smith0892@gmail.com>
2021-09-18 19:50:57 -1000

    Another typo
    OTP

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210918.1945';
+const cacheName = 'chirpy-20210918.1950';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
