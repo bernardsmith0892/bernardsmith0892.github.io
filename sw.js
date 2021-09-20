cdb0d43bccd40509ffbf3737ed8500fadbfb1ee9
bernardsmith0892 <bernard.smith0892@gmail.com>
2021-09-20 00:15:32 -1000

    Added intro and cert to Hacktivity CTF writeup

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210919.1959';
+const cacheName = 'chirpy-20210920.0014';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
