1d403af198f89b81dcb49ee87eabcacd3ddcadb4
bernardsmith0892 <bernard.smith0892@gmail.com>
2021-10-25 01:15:51 -1000

    Rebuilt website with updated Jekyll and added retirement calc to projects page

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210920.0014';
+const cacheName = 'chirpy-20211025.0111';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
